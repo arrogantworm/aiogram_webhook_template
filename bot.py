5ec481c58f8b0816b7a38d7b1549943e129133eb
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-03 02:51:15 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher
-from aiogram.client.session.aiohttp import AiohttpSession
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
 from core.settings import config
 from aiohttp import web
 import asyncio
@@ -8,12 +7,7 @@ import logging
 from core.handlers import basic
 
 
-async def on_startup(bot: Bot, dp: Dispatcher):
-    await bot.set_webhook(
-        url=config.URL_DOMAIN + config.URL_PATH,
-        drop_pending_updates=True,
-        allowed_updates=dp.resolve_used_update_types()
-    )
+async def on_startup(bot: Bot):
     await bot.send_message(config.ADMIN_ID, 'Бот запущен')
 
 
@@ -26,9 +20,7 @@ async def start():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - [%(levelname)s] - %(name)s - '
                                '(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s')
-    session = AiohttpSession()
-    bot_settings = {'session': session, 'parse_mode': 'HTML'}
-    bot = Bot(config.BOT_TOKEN.get_secret_value(), **bot_settings)
+    bot = Bot(config.BOT_TOKEN.get_secret_value())
     dp = Dispatcher()
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
@@ -37,11 +29,19 @@ async def start():
     dp.include_router(basic.router)
 
     try:
-
+        await bot.set_webhook(
+            url=config.URL_DOMAIN + config.URL_PATH,
+            drop_pending_updates=True,
+            allowed_updates=dp.resolve_used_update_types()
+        )
         app = web.Application()
         SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=config.URL_PATH)
-        setup_application(app, dp, bot=bot)
-        web.run_app(app, host=config.SERVER_HOST, port=config.SERVER_PORT)
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, host=config.SERVER_HOST, port=config.SERVER_PORT)
+        await site.start()
+
+        await asyncio.Event().wait()
     finally:
         await bot.session.close()
 
